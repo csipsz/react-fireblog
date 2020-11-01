483a769e37bd4d3f5c9807fd8d74f70f0ce2f1a0
Luca <lucakanyo@protonmail.com>
2020-11-01 08:42:58 -0500

    firebase configuration

--- src/index.js
+++ src/index.js
@@ -3,12 +3,21 @@ import ReactDOM from 'react-dom';
 import './index.css';
 import App from './App';
 import reportWebVitals from './reportWebVitals';
-import {createStore, applyMiddleware} from 'redux';
+import {createStore, applyMiddleware, compose} from 'redux';
 import rootReducer from './store/reducers/rootReducer';
 import { Provider } from 'react-redux';
 import thunk from 'redux-thunk';
+import { reduxFireStore, getFireStore } from 'redux-firestore'
+import { reactReduxFirebase, getFireBase } from 'react-redux-firebase'
+import fbConfig from './config/fbConfig'
 
-const store = createStore(rootReducer, applyMiddleware(thunk));
+const store = createStore(rootReducer, 
+  compose(
+    applyMiddleware(thunk.withExtraArgument({getFireBase, getFireStore})),
+    reduxFireStore(),
+    reactReduxFirebase()
+)
+);
 
 ReactDOM.render(
     <Provider store={store}>
